2e91af37ee6b3db382bfe52f3b1560b6a61624ce
Barry Keenan <barry.keenan@resn.co.nz>
2022-09-03 09:27:49 +1200

    Add single time slider to drive sun position

--- src/components/settings-panel.js
+++ src/components/settings-panel.js
@@ -10,10 +10,9 @@ class SettingsPanel {
 
         this.gui = new dat.GUI();
 
-        this.heightScale = 0.2;
-        this.bouncingSpeed = 0.04;
+        this.time = 0;
 
-        this.showRay = false;
+        const mapRange = (value, x1, y1, x2, y2) => ((value - x1) * (y2 - x2)) / (y1 - x1) + x2;
 
         // const cameraFolder = this.gui.addFolder('Camera');
         // cameraFolder.add(this.camera.position, 'x', -500, 500);
@@ -24,21 +23,41 @@ class SettingsPanel {
         const keyLight = this.scene.getObjectByName('keyLight');
         const keyLightHelper = this.scene.getObjectByName('keyLight-helper');
         if (keyLight) {
-            const lightsFolder = this.gui.addFolder('Key light');
+            const lightControlsFolder = this.gui.addFolder('Light controls');
 
-            // lightsFolder.add(keyLight, 'intensity', 0, 10, 0.1);
-
-            lightsFolder.add(keyLight.position, 'x', -1000, 1000).onChange(() => {
+            lightControlsFolder.add(keyLight.position, 'x', -1500, 1500).onChange(() => {
                 this.updateLight(keyLight, keyLightHelper);
             });
-            lightsFolder.add(keyLight.position, 'y', 100, 1000).onChange(() => {
+            lightControlsFolder.add(keyLight.position, 'y', 0, 700).onChange(() => {
                 this.updateLight(keyLight, keyLightHelper);
             });
-            // lightsFolder.add(keyLight.position, 'z', -1000, 1000).onChange(() => {
-            //     this.updateLight(keyLight, keyLightHelper);
-            // });
 
-            lightsFolder.open();
+            lightControlsFolder.add(keyLight, 'intensity', 0, 10, 0.1);
+
+            lightControlsFolder.open();
+
+            // time
+            const timeFolder = this.gui.addFolder('Time of day');
+
+            timeFolder.add(this, 'time', 0, 1, 0.01).onChange(() => {
+                const sinValue = Math.sin(Math.PI * this.time);
+
+                keyLight.position.x = mapRange(this.time, 0, 1, -1500, 1500);
+                keyLight.position.y = mapRange(sinValue, 0, 1, 100, 700);
+                keyLight.intensity = mapRange(sinValue, 0, 1, 2, 10);
+
+                this.updateLight(keyLight, keyLightHelper);
+            });
+
+            timeFolder.open();
+
+            // output
+            const outputFolder = this.gui.addFolder('Sun position');
+
+            outputFolder.add(keyLight.position, 'x').name('azimuth (x)').listen();
+            outputFolder.add(keyLight.position, 'y').name('zenith (y)').listen();
+
+            outputFolder.open();
         }
 
         // const debugFolder = this.gui.addFolder('Debug');
